a376f8f20060fbe3cc4ad1ab6290d3343ebcf7ec
Navin <nntheprince866@gmail.com>
2015-04-29 14:49:10 -0500

    Update Prefetcher.py

--- gem5-stable/src/mem/cache/prefetch/Prefetcher.py
+++ gem5-stable/src/mem/cache/prefetch/Prefetcher.py
@@ -79,10 +79,10 @@ class MarkovPrefetcher(BasePrefetcher):
     cxx_class = 'MarkovPrefetcher'
     cxx_header = "mem/cache/prefetch/markov.hh"
 
-#class DeltaPrefetcher(BasePrefetcher):
-#    type = 'DeltaPrefetcher'
-#    cxx_class = 'DeltaPrefetcher'
-#    cxx_header = "mem/cache/prefetch/delta.hh"
+class DeltaPrefetcher(BasePrefetcher):
+    type = 'DeltaPrefetcher'
+    cxx_class = 'DeltaPrefetcher'
+    cxx_header = "mem/cache/prefetch/delta.hh"
 
 class TaggedPrefetcher(BasePrefetcher):
     type = 'TaggedPrefetcher'
